443756cb305fd48c56f1d206e96277dfa79f4e00
alayedomar <osalaayed@gmail.com>
2023-12-01 00:20:28 +0300

    stateless main

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -4,32 +4,9 @@ import 'package:mefa8/const.dart';
 import 'package:mefa8/widgets/home/index.dart';
 import 'package:mefa8/widgets/home/services.dart';
 
-class Main extends StatefulWidget {
+class Main extends StatelessWidget {
   const Main({super.key});
 
-  @override
-  State<Main> createState() => _MainState();
-}
-
-class _MainState extends State<Main> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 390),
-    );
-    _animation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.fastOutSlowIn,
-    ))
-      ..addListener(() => setState(() {}));
-    super.initState();
-  }
-
-  
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
